a23acb79e9b5f2bb27024dd74248b17eb7206427
Piumi Dissanayake <dissanayakepiumi75@gmail.com>
2024-04-22 20:06:12 +0530

    modify bottom nav bar

--- planting_distribution/lib/components/my_bottom_nav_bar.dart
+++ planting_distribution/lib/components/my_bottom_nav_bar.dart
@@ -1,8 +1,23 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:planting_distribution/constants.dart';
 
 class MyBottomNavBar extends StatefulWidget{
   const MyBottomNavBar ({
-    
-  })
+    Key key,
+  }):super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      padding: EdgeInsets.only(
+        left: kDefaultPadding *2,
+        right: kDefaultPadding *2,
+        bottom: kDefaultPadding,
+      ),
+      height: 80,
+      
+    )
+  }
 }
